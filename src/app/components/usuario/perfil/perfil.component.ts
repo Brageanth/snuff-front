7414ec82eff23ba0bd5a9e85f6c387fc8f03af58
Brageanth Palencia <brageanth@gmail.com>
2019-02-07 15:25:33 -0500

    cargo pintura

--- src/app/components/usuario/perfil/perfil.component.ts
+++ src/app/components/usuario/perfil/perfil.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { AppComponent } from 'src/app/app.component';
+import { LoginService } from 'src/app/services/login.service';
+import { Usuario } from 'src/app/models/usuario';
 
 @Component({
   selector: 'app-perfil',
@@ -10,16 +12,37 @@ import { AppComponent } from 'src/app/app.component';
 })
 export class PerfilComponent implements OnInit {
 
+  users: Array<Usuario>;
+  token: string;
+  usuario: Usuario;
+
   constructor(
     private appComponent: AppComponent,
     private cookieService: CookieService,
-    private router: Router
+    private router: Router,
+    private loginService: LoginService
   ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.comprobarLogin();
+    this.users = <Usuario[]> await this.loginService.getUsuarios();
+    this.usuario = this.buscarUser(this.token);
+    this.appComponent.typeNav(true);
+  }
+
+  comprobarLogin() {
     if (!this.cookieService.check('Token')) {
       this.router.navigate(['/login']);
+    } else {
+      this.token = this.cookieService.get('Token');
     }
-    this.appComponent.typeNav(true);
+  }
+
+  buscarUser(correo: string) {
+    for (const user of this.users) {
+      if (correo === user.correo) {
+        return user;
+      }
+    }
   }
 }
