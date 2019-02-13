934e75899b0fe2e1a0caecc5ea3d0a27a2f6bc96
Brageanth Palencia <brageanth@gmail.com>
2019-02-13 15:58:13 -0500

    Camiseta 3D

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -142,8 +142,12 @@ init();
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
+    console.log(this.containerCamisa);
+
     var renderer = new THREE.WebGLRenderer({canvas : this.containerCamisa});
     renderer.setSize( window.innerWidth, window.innerHeight );
+
+    console.log(renderer);
 
     var geometry = new THREE.BoxGeometry( 1, 1, 1 );
     var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
