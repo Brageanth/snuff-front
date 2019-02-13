d6e0ad3e81ab112d4c2c684f52d4ae907804d34b
Brageanth Palencia <brageanth@gmail.com>
2019-02-13 16:19:58 -0500

    Camiseta 3D

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -140,7 +140,7 @@ init();
   importCamiseta() {
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-
+    console.log(this.imgBack);
     console.log(this.containerCamisa);
 
     var renderer = new THREE.WebGLRenderer({canvas : (this.containerCamisa.nativeElement as HTMLCanvasElement)});
