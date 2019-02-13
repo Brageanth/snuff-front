29c41effd7a520c46b6be219e3380b860c8e5449
Brageanth Palencia <brageanth@gmail.com>
2019-02-12 23:50:16 -0500

    camiseta 3d

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -102,7 +102,7 @@ var loadOBJ = function(){
 	var loader = new THREE.OBJLoader( manager );
   
 	//Launch loading of the obj file, addBananaInScene is the callback when it's ready 
-	loader.load( '../../../assets/3d/Camiseta.obj', addBananaInScene);
+	loader.load( 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/127738/banana.obj', addBananaInScene);
 
 };
 
